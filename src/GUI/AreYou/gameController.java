21726430f99f7b957027301745fd4c17591baae8
guyshargo <shargo501@gmail.com>
2025-02-24 17:46:37 +0200

    defining files

---
+++ src/GUI/AreYou/gameController.java
@@ -0,0 +1,35 @@
+package GUI.AreYou;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.AnchorPane;
+
+import java.util.Random;
+
+public class gameController {
+    @FXML private Button yesButton;
+    @FXML private Button noButton;
+
+    private final Random randomLocation = new Random();
+
+    @FXML
+    private void handleYes() {
+        Stage stage = (Stage) yesButton.getScene().getWindow();
+        AnchorPane root = new AnchorPane();
+        Scene scene = new Scene(root, 400, 300);
+        Text congratsText = new Text(140, 150, "Congrats faggot");
+        root.getChildren().add(congratsText);
+        stage.setScene(scene);
+    }
+
+    @FXML
+    private void handleNoHover() {
+        double newX = randomLocation.nextDouble() * (noButton.getScene().getWidth() - noButton.getWidth());
+        double newY = randomLocation.nextDouble() * (noButton.getScene().getHeight() - noButton.getHeight());
+        noButton.setLayoutX(newX);
+        noButton.setLayoutY(newY);
+    }
+}
